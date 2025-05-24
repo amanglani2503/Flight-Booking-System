a4579e491ff766e5fe743562e24b7a6a57d12bfe
amanglani2503 <ambermanglani8871@gmail.com>
2025-05-24 18:37:49 +0530

    Add. Added .gitignore file

--- user-service/src/main/java/com/example/user_service/model/UserRegistration.java
+++ user-service/src/main/java/com/example/user_service/model/UserRegistration.java
@@ -1,7 +1,10 @@
 package com.example.user_service.model;
 
-
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,12 +22,18 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Name cannot be blank")
+    @Size(min = 2, max = 50, message = "Name must be between 2 and 50 characters")
     @Column(nullable = false)
     private String name;
 
+    @NotBlank(message = "Email cannot be blank")
+    @Email(message = "Invalid email format")
     @Column(unique = true, nullable = false)
     private String email;
 
+    @NotBlank(message = "Password cannot be blank")
+    @Size(min = 6, message = "Password must be at least 6 characters long")
     @Column(nullable = false)
     private String password;
 
